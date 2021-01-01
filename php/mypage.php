f8062f44aa40bbeaff3507a1528cd94a18ca5dfe
kei610 <mamedaihuku610@gmail.com>
2021-01-01 18:03:16 +0900

    セクションと仮のカードを追加

--- php/mypage.php
+++ php/mypage.php
@@ -33,46 +33,117 @@
     <main class="main">
         <div class="container">
 
-            <h2 class="page-title">マイページ</h2>
-
-            <div class="mypage-header">
-                <span class="post-count">post</span>
-                <form method="POST" class="sort-form">
-                    <div class="form-group">
-                        <select name="sort" id="" class="select">
-                            <option value="">日付の新しい順</option>
-                            <option value="">日付の古い順</option>
-                        </select>
+            <section class="section">
+                <div class="section-header">
+                    <h2 class="page-title">マイページ</h2>
+                    <div class="section-inner">
+                        <form method="POST" class="sort-form">
+                            <div class="form-group">
+                                <select name="sort" id="" class="select">
+                                    <option value="">日付の新しい順</option>
+                                    <option value="">日付の古い順</option>
+                                </select>
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
-            
-            <div class="card-wrap">
-                <div class="card">
-                    <a href="" class="card-link">
-                        <div class="card-head">
-                            <img src="" alt="" class="card-img">
+                </div>
+                
+                
+                <div class="card-wrap">
+                    <div class="card">
+                        <a href="" class="card-date">
+                            2020/04/23
+                        </a>
+                        <a href="" class="card-link">
+                            <div class="card-head">
+                                <img src="../img/arashiyama.jpg" alt="" class="card-img">
+                            </div>
+                            <div class="card-foot">
+                                <span class="card-comment"></span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card">
+                        <a href="" class="card-date">
+                            2020/04/23
+                        </a>
+                        <a href="" class="card-link">
+                            <div class="card-head">
+                                <img src="../img/arashiyama.jpg" alt="" class="card-img">
+                            </div>
+                            <div class="card-foot">
+                                <span class="card-comment"></span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card">
+                        <a href="" class="card-date">
+                            2020/04/23
+                        </a>
+                        <a href="" class="card-link">
+                            <div class="card-head">
+                                <img src="../img/arashiyama.jpg" alt="" class="card-img">
+                            </div>
+                            <div class="card-foot">
+                                <span class="card-comment"></span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card">
+                        <a href="" class="card-date">
+                            2020/04/23
+                        </a>
+                        <a href="" class="card-link">
+                            <div class="card-head">
+                                <img src="../img/arashiyama.jpg" alt="" class="card-img">
+                            </div>
+                            <div class="card-foot">
+                                <span class="card-comment"></span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card">
+                        <a href="" class="card-date">
+                            2020/04/23
+                        </a>
+                        <a href="" class="card-link">
+                            <div class="card-head">
+                                <img src="../img/arashiyama.jpg" alt="" class="card-img">
+                            </div>
+                            <div class="card-foot">
+                                <span class="card-comment"></span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card">
+                        <a href="" class="card-date">
+                            2020/04/23
+                        </a>
+                        <a href="" class="card-link">
+                            <div class="card-head">
+                            <img src="../img/arashiyama.jpg" alt="" class="card-img">
                         </div>
                         <div class="card-foot">
                             <span class="card-comment"></span>
                         </div>
                     </a>
                 </div>
-            </div>
-
-            <div class="post-photo-link">
-                <a href="post_photo.php" class="post-photo-link-btn"></a>
-            </div>
-
+            
+                <div class="post-photo-link">
+                    <a href="post_photo.php" class="post-photo-link-btn"></a>
+                </div>
+            
+            </section> 
+            
         </div>
     </main>
-
+    
     <footer class="footer">
         <div class="footer-container">
             <div class="footer-logo">
                 <!-- <img src="" alt=""> -->
             </div>
-
+            
             <nav class="footer-nav">
                 <ul class="footer-nav-menu">
                     <li class="footer-nav-menu-item"><a href="" class="footer-nav-menu-item-link">利用規約</a></li>
