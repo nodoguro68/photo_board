e5746dd48b6b1f6494d992c32b3d8c7eee7546ae
kei610 <mamedaihuku610@gmail.com>
2021-01-05 12:38:41 +0900

    フッターをテンプレート化

---
+++ php/templete/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="footer-container">
+        <div class="footer-logo">
+            <!-- <img src="" alt=""> -->
+        </div>
+
+        <nav class="footer-nav">
+            <ul class="footer-nav-menu">
+                <li class="footer-nav-menu-item"><a href="" class="footer-nav-menu-item-link">利用規約</a></li>
+                <li class="footer-nav-menu-item"><a href="" class="footer-nav-menu-item-link">プライバシーポリシー</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
