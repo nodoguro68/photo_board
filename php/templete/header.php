c334e9f9f27434f41e801f20541a0f557865805d
kei610 <mamedaihuku610@gmail.com>
2021-01-05 12:39:35 +0900

    ヘッダーをテンプレート化 ログインしているかどうかでナビゲーションの項目を変える

---
+++ php/templete/header.php
@@ -0,0 +1,21 @@
+<header class="header">
+    <div class="header-container">
+        <div class="header-logo">
+            <h1></h1>
+        </div>
+
+        <nav class="header-nav">
+            <ul class="header-nav-menu">
+                <li class="header-nav-menu-item"><a href="index.php" class="header-nav-menu-item-link">トップページ</a></li>
+                <?php if(empty($_SESSION['user_id']) && $page === 'login'): ?>
+                    <li class="header-nav-menu-item"><a href="signup.php" class="header-nav-menu-item-link">ユーザー登録</a></li>
+                <?php elseif(empty($_SESSION['user_id']) && $page !== 'login'): ?>
+                    <li class="header-nav-menu-item"><a href="login.php" class="header-nav-menu-item-link">ログイン</a></li>
+                <?php else: ?>
+                    <li class="header-nav-menu-item"><a href="" class="header-nav-menu-item-link">ログアウト</a></li>
+                    <li class="header-nav-menu-item"><a href="mypage.php" class="header-nav-menu-item-link">マイページ</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </div>
+</header>
