4e14ccf5640b298406616374ab9697138616b550
kei610 <mamedaihuku610@gmail.com>
2021-01-06 21:31:18 +0900

    ログアウト処理だけに変更

--- php/logout.php
+++ php/logout.php
@@ -4,28 +4,3 @@ require_once 'config.php';
 
 logout();
 
-?>
-
-<?php 
-
-$page_title = 'ログアウト';
-
-require_once 'templete/head.php';
-require_once 'templete/header.php';
-
-?>
-
-    <main class="main">
-        <div class="container">
-            
-            
-
-        </div>
-    </main>
-
-    <?php require_once 'templete/footer.php';?>
-    
-    <script src="../js/main.js"></script>
-    <script src="../js/form.js"></script>
-</body>
-</html>
