f6fe596c4339be7044bda29b61557dbe203a1141
kei610 <mamedaihuku610@gmail.com>
2021-01-06 21:14:55 +0900

    メニュー表示修正

--- php/templete/header.php
+++ php/templete/header.php
@@ -7,13 +7,14 @@
         <nav class="header-nav">
             <ul class="header-nav-menu">
                 <li class="header-nav-menu-item"><a href="index.php" class="header-nav-menu-item-link">トップページ</a></li>
-                <?php if(empty($_SESSION['user_id']) && $page_title === 'ログイン'): ?>
-                    <li class="header-nav-menu-item"><a href="signup.php" class="header-nav-menu-item-link">ユーザー登録</a></li>
-                <?php elseif(empty($_SESSION['user_id']) && $page_title !== 'ログイン'): ?>
-                    <li class="header-nav-menu-item"><a href="login.php" class="header-nav-menu-item-link">ログイン</a></li>
-                <?php else: ?>
-                    <li class="header-nav-menu-item"><a href="" class="header-nav-menu-item-link">ログアウト</a></li>
+
+                <?php if(!empty($_SESSION['login_user'])) :?>
                     <li class="header-nav-menu-item"><a href="mypage.php" class="header-nav-menu-item-link">マイページ</a></li>
+                    <li class="header-nav-menu-item"><a href="" class="header-nav-menu-item-link">ログアウト</a></li>
+                <?php elseif(empty($_SESSION['login_user']) && $page_title === 'ログイン'): ?>
+                    <li class="header-nav-menu-item"><a href="signup.php" class="header-nav-menu-item-link">ユーザー登録</a></li>
+                <?php else: ?>
+                    <li class="header-nav-menu-item"><a href="login.php" class="header-nav-menu-item-link">ログイン</a></li>
                 <?php endif; ?>
             </ul>
         </nav>
