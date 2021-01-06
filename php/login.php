bb9cf91cb48d036e7d5ab828b6e3b71472cfef19
kei610 <mamedaihuku610@gmail.com>
2021-01-06 21:14:13 +0900

    ログイン機能 ヘッダー、フッターテンプレート化

--- php/login.php
+++ php/login.php
@@ -1,34 +1,55 @@
 <?php 
 
+require_once 'config.php';
+
+if (!empty($_POST)) {
+
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    validationEmpty('email');
+    validationEmpty('pass');
+
+    if(empty($err_msg)){
+
+        // Eメール
+        validationMaxLen($email,'email',255);
+        validationEmail($email);
+        
+        // パスワード
+        validationPass($pass);
+        validationMaxLen($pass,'pass',255);
+        validationMinLen($pass,'pass');
+
+        if(empty($err_msg)){
+
+            $dbh = dbConnect();
+
+            $result = login($email,$pass);
+
+            if($result){
+
+                header('Location: mypage.php');
+
+            } else {
+                $err_msg['email'] = 'メールアドレスかパスワードのどちらかが間違っています';
+            }
+
+        }
+    }
+}
 
 
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ログイン</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
 
-<body>
+<?php 
 
-    <header class="header">
-        <div class="header-container">
-            <div class="header-logo">
-                <h1>ロゴ</h1>
-            </div>
+$page_title = 'ログイン';
 
-            <nav class="header-nav">
-                <ul class="header-nav-menu">
-                    <li class="header-nav-menu-item"><a href="index.php" class="header-nav-menu-item-link">トップページ</a></li>
-                    <li class="header-nav-menu-item"><a href="signup.php" class="header-nav-menu-item-link">ユーザー登録</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+require_once 'templete/head.php';
+require_once 'templete/header.php';
+
+?>
 
     <main class="main">
         <div class="container">
@@ -41,13 +62,13 @@
                 <div class="form-main">
                     <div class="form-group">
                         <label class="form-label">メールアドレス</label>
-                        <input type="text" name="email" class="form-input">
-                        <span class="err-msg"></span>
+                        <input type="text" name="email" value="<?php echo post('email'); ?>" class="form-input" autofocus>
+                        <span class="err-msg"><?php echo err('email'); ?></span>
                     </div>
                     <div class="form-group">
                         <label class="form-label">パスワード<span class="note">*半角英数字8文字以上で入力してください</span></label>
-                        <input type="password" name="pass" class="form-input">
-                        <span class="err-msg"></span>
+                        <input type="password" name="pass" value="<?php echo post('pass'); ?>" class="form-input">
+                        <span class="err-msg"><?php echo err('pass'); ?></span>
                     </div>
                     <div class="form-group-btn">
                         <input type="submit" value="ログイン" class="btn-submit">
@@ -58,20 +79,7 @@
         </div>
     </main>
 
-    <footer class="footer">
-        <div class="footer-container">
-            <div class="footer-logo">
-                <!-- <img src="" alt=""> -->
-            </div>
-
-            <nav class="footer-nav">
-                <ul class="footer-nav-menu">
-                    <li class="footer-nav-menu-item"><a href="" class="footer-nav-menu-item-link">利用規約</a></li>
-                    <li class="footer-nav-menu-item"><a href="" class="footer-nav-menu-item-link">プライバシーポリシー</a></li>
-                </ul>
-            </nav>
-        </div>
-    </footer>
+    <?php require_once 'templete/footer.php';?>
     
     <script src="../js/main.js"></script>
     <script src="../js/form.js"></script>
