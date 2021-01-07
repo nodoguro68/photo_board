a147db525aabfa6c7fcdbbc431967a9a6659ded1
kei610 <mamedaihuku610@gmail.com>
2021-01-07 20:03:24 +0900

    投稿フォーム完成 投稿機能完成

--- php/post_photo.php
+++ php/post_photo.php
@@ -1,7 +1,34 @@
 <?php 
 
+require_once 'config.php';
 
+if(!empty($_POST) && !empty($_FILES)){
 
+    $photo_file = $_FILES['photo'];
+    $photo_file_name = $_FILES['photo']['name'];
+    $photo_tmp_path = $_FILES['photo']['tmp_name'];
+    $photo_file_err = $_FILES['photo']['error'];
+    $photo_file_size = $_FILES['photo']['size'];
+    $upload_dir = '/Applications/MAMP/htdocs/photo_board/uploads/';
+    $save_file_name = date('YmdHis').$photo_file_name;
+    $save_path = $upload_dir.$save_file_name;
+    
+    $comment = $_POST['comment'];
+
+    validationFileSize($photo_file_size,$photo_file_err);
+    validationFileExt($photo_file_name);
+    
+    validationMaxLen($comment,'comment',200);
+    
+    if(empty($err_msg)){
+        
+        uploadFile($photo_tmp_path,$save_path,$save_file_name);
+        createPost($_SESSION['login_user']['id'],$photo_file_name,$save_path,$comment);
+
+        header('Location: index.php');
+    }
+
+}
 
 ?>
 
@@ -11,28 +38,30 @@ $page_title = '新規投稿ページ';
 
 require_once 'templete/head.php';
 require_once 'templete/header.php';
-
 ?>
 
     <main class="main">
         <div class="container">
 
-            <form method="POST" class="post-photo-form">
+            <form method="POST" class="post-photo-form" enctype="multipart/form-data">
                 <div class="form-header">
                     <h2 class="form-title">新規投稿</h2>
                 </div>
                 <div class="form-main">
                     <div class="form-group">
-                        <input type="file" name="" id="" class="img">
+                        <input type="hidden" name="MAX_FILE_SIZE" value="1048576">
+                        <input type="file" name="photo" class="img">
+                        <span class="err-msg"><?php echo err('photo'); ?></span>
                     </div>
                     <div class="form-group">
-                        <textarea name="comment" id="" class="comment" placeholder="コメントを入力"></textarea>
+                        <textarea name="comment" id="" class="comment" placeholder="コメントを入力"><?php echo post('comment'); ?></textarea>
+                        <span class="err-msg"><?php echo err('comment'); ?></span>
                     </div>
                     <div class="counter-area">
                         <span class="count">0</span>/200
                     </div>
                     <div class="form-group-btn">
-                        <input type="submit" value="投稿" class="btn-submit">
+                        <input type="submit" name="post" value="投稿" class="btn-submit">
                     </div>
                 </div>
             </form>
@@ -40,7 +69,7 @@ require_once 'templete/header.php';
         </div>
     </main>
 
-    <?php require_once 'templete/footer.php'; ?>
+<?php require_once 'templete/footer.php'; ?>
     
     <script src="../js/main.js"></script>
     <script src="../js/form.js"></script>
